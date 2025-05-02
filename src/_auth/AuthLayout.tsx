c956c08f5181e0bc18dfde80ee38275c137d069c
Anant Agarwal <115874061+AnantAgarwaL11@users.noreply.github.com>
2025-05-02 12:14:28 +0530

    Add files via upload

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Outlet ,  Navigate } from "react-router-dom"
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+  return (
+    <>
+    {isAuthenticated ? (
+      <Navigate to='/' />
+    ) : ( 
+      <>
+      <section className="flex flex-1 justify-center items-center flex-col py-10 ">
+        <Outlet/>
+      </section>
+
+      <img src="/public/assets/images/side-img.png"
+      // i can add hidden class in the below line 
+      alt="logo"  className=" xl:block h-screen w-1/2 object-cover bg-no-repeat"/>
+      </>
+
+    )}
+    
+    
+    </>
+    
+  )
+}
+
+export default AuthLayout
